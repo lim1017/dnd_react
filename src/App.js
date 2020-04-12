aa2b1350e4061c82f5f76d786d57412ff5e18efe
tom <lim1017@hotmail.com>
2020-04-12 12:09:42 -0400

    board

--- src/App.js
+++ src/App.js
@@ -2,17 +2,15 @@ import React from 'react';
 import './App.css';
 import Knight from "./components/knight/knight.jsx"
 import Square from "./components/square/square.jsx"
+import Board from "./components/board/board.jsx"
 
 
 function App() {
   return (
-    <div className="App">
+    <div>
       <h1>DRAG AND DROP</h1>
-      <p>
-        <Square black >
-          <Knight />
-        </Square>
-      </p>
+      <Board knightPosition={[0, 1]} />
+
 
     </div>
   );
